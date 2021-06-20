9148ac42fa6d29b22d7a16ab3e8637616621b5a9
cyx200902 <62412516+cyx200902@users.noreply.github.com>
2021-06-20 17:52:35 +0800

    Add files via upload

---
+++ leimo/a.py
@@ -0,0 +1,13 @@
+import sys
+import time
+f=sys.argv[0]
+if f !="C:\Program Files (x86)\快速下载器\快速下载器.exe":
+    print("oh no oh no,oh no no no no no")
+    print(sys.argv[0])
+    time.sleep(20)
+else:
+    print("ok")
+    time.sleep(20)
+
+
+#http://www.eshzp.com/news/4947.html
